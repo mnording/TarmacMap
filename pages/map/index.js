1e253a2c981f9e7506cd25d79358134005baba8f
Mattias Nording <mattiasnording@gmail.com>
2021-11-27 16:14:24 +0100

    finally solved cache issues

--- pages/map/index.js
+++ pages/map/index.js
@@ -8,8 +8,8 @@ class Map extends React.Component {
       lat:  59.37215372800212,
       lng: 16.51307543068856,
     },
-    currentMap: 'https://www.google.com/maps/d/u/0/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c'
-    
+    currentMap: 'https://www.google.com/maps/d/u/0/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c&forcekml=1',
+   
   };
   kmzLayeMattiasr = null;
 
@@ -24,19 +24,20 @@ class Map extends React.Component {
     const bounds = new google.maps.LatLngBounds();
    
     this.kmzLayeMattiasr = new google.maps.KmlLayer(currentMap, {
-      suppressInfoWindows: true,
+      suppressInfoWindows: false,
       preserveViewport: false,
       map: this.map
     });
    // this.kmzLayeMattiasr.setMap(this.map);
   };
   handleAttachGoogleMap = () => {
-    const { defaultCenter } = this.state;
+    const { defaultCenter,mapStyle } = this.state;
     //this.kmzLayeMattiasr = new google.maps.KmlLayer(this.state.currentMap);
     this.map = new google.maps.Map(document.getElementById("google-map"), {
       center: defaultCenter,
       zoom: 15,
     });
+    this.map.set('styles',mapStyle);
     setTimeout(() => {
       this.handleDrawMarkers();
     }, 2000);
@@ -48,8 +49,13 @@ class Map extends React.Component {
       zoom: 100,
     });
     this.kmzLayeMattiasr.setMap(null);
-    this.kmzLayeMattiasr = new google.maps.KmlLayer(event.target.value);
-    this.kmzLayeMattiasr.setMap(this.map);
+    console.log("settings map to "+event.target.value)
+    this.kmzLayeMattiasr = new google.maps.KmlLayer(event.target.value, {
+      suppressInfoWindows: false,
+      preserveViewport: false,
+      map: this.map
+    });
+;
     
    
 };
@@ -59,8 +65,8 @@ class Map extends React.Component {
         <div id="google-map" />
       </StyledMap>
       <select  style={{display:"flex",margin:'0 auto',padding:'10px'}} onChange={this.change}>
-        <option value="https://www.google.com/maps/d/u/0/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c">Larsa</option>
-        <option value="https://www.google.com/maps/d/u/0/kml?mid=1g8DMXDvq0gH-SbKzv-JR5WVTX78lni7J">Jocke</option></select>
+        <option value="https://www.google.com/maps/d/u/0/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c&nl=1&forcekml=1">Larsa</option>
+        <option value="https://www.google.com/maps/d/u/0/kml?mid=1g8DMXDvq0gH-SbKzv-JR5WVTX78lni7J&nl=1&forcekml=1">Jocke</option></select>
       </div>
     );
   }
