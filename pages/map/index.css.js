1652e1c9afc18d1498bf6c1f5a2505871df386e6
Mattias Nording <mattiasnording@gmail.com>
2021-11-27 11:33:32 +0100

    adding selector for maps

--- pages/map/index.css.js
+++ pages/map/index.css.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export default styled.div`
   #google-map {
     width: 100%;
-    height: 100vh;
+    height: 90vh;
   }
 `;
--- pages/map/index.js
+++ pages/map/index.js
@@ -8,9 +8,10 @@ class Map extends React.Component {
       lat:  59.37215372800212,
       lng: 16.51307543068856,
     },
-    
+    currentMap: 'https://www.google.com/maps/d/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c&nl=1&cid=mp&cv=gFJlqtzGxiQ.sv.'
     
   };
+  kmzLayeMattiasr = null;
 
   componentDidMount() {
     document.body.classList.add("is-map");
@@ -19,15 +20,16 @@ class Map extends React.Component {
 
   componentWillUnmount() {  document.body.classList.remove("is-map"); }
   handleDrawMarkers = () => {
-    const { markers } = this.state;
+    const { markers,currentMap } = this.state;
     const bounds = new google.maps.LatLngBounds();
    
-    var kmzLayeMattiasr = new google.maps.KmlLayer('https://www.google.com/maps/d/u/0/kml?mid=1g8DMXDvq0gH-SbKzv-JR5WVTX78lni7J&nl=1&cid=mp&cv=gFJlqtzGxiQ.sv.');
+    this.kmzLayeMattiasr = new google.maps.KmlLayer(currentMap);
     var kmzLayer = new google.maps.KmlLayer('https://www.google.com/maps/d/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c&nl=1&cid=mp&cv=gFJlqtzGxiQ.sv.');
-    kmzLayeMattiasr.setMap(this.map);
+   this.kmzLayeMattiasr.setMap(this.map);
   };
   handleAttachGoogleMap = () => {
     const { defaultCenter } = this.state;
+    this.kmzLayeMattiasr = new google.maps.KmlLayer(this.state.currentMap);
     this.map = new google.maps.Map(document.getElementById("google-map"), {
       center: defaultCenter,
       zoom: 10,
@@ -36,12 +38,20 @@ class Map extends React.Component {
       this.handleDrawMarkers();
     }, 2000);
   };
-
+  change= (event) =>{
+    this.kmzLayeMattiasr = new google.maps.KmlLayer(event.target.value);
+    this.kmzLayeMattiasr.setMap(this.map);
+    this.setState({value: event.target.value});
+};
   render() {
-    return (
+    return (<div>
       <StyledMap>
         <div id="google-map" />
       </StyledMap>
+      <select  onChange={this.change}>
+        <option value="https://www.google.com/maps/d/kml?mid=1VeRAB3VxhihKjIroI-5r0Gt922ebT57c&nl=1&cid=mp&cv=gFJlqtzGxiQ.sv.">Larsa</option>
+        <option value="https://www.google.com/maps/d/u/0/kml?mid=1g8DMXDvq0gH-SbKzv-JR5WVTX78lni7J&nl=1&cid=mp&cv=gFJlqtzGxiQ.sv.">Jocke</option></select>
+      </div>
     );
   }
 }
